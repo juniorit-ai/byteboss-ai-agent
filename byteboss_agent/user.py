7ce400e9f4873bc92885c64856c109fc820e65dd
ezhomelabs <haiwei.he@gmail.com>
2024-07-03 21:10:01 +1000

    Do not auto run commands in the juniorit container

--- byteboss_agent/user.py
+++ byteboss_agent/user.py
@@ -1,13 +1,19 @@
+import os
+
 class User:
     @staticmethod
     def get_user_response(prompt):
         return input(prompt)
     
     @staticmethod
-    def confirm_action(prompt, commands):
-        print(prompt)
+    def confirm_action(description, commands):
+        print(description)
         for command in commands:
             print(command)
+            
+        if os.getenv('JUNIORIT_CONTAINER_TOKEN') is not None:
+            print("Please execute the commands above manually.")
+            return False
         
         user_response = input("Do you want to proceed with these commands? (yes/No): ")
         if user_response.lower() not in ('yes', 'y'):
